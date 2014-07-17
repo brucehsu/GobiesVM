6f506e90491b1e7308fa9e198b0f462b97150f1d
Szu-Kai Hsu (brucehsu) <brucehsu@gmail.com>
2014-07-17 11:01:49 +0800

    [RString] length functions implemented

--- src/rstring.go
+++ src/rstring.go
@@ -10,6 +10,8 @@ func initRString() *RObject {
 	// RString method initialization
 	obj.methods["new"] = &RMethod{gofunc: RString_new}
 	obj.methods["to_s"] = &RMethod{gofunc: RString_to_s}
+	obj.methods["size"] = &RMethod{gofunc: RString_length}
+	obj.methods["len"] = &RMethod{gofunc: RString_length}
 
 	return obj
 }
@@ -32,4 +34,11 @@ func RString_new(vm *GobiesVM, receiver Object, v []Object) Object {
 func RString_to_s(vm *GobiesVM, receiver Object, v []Object) Object {
 	obj := receiver.(RObject)
 	return obj.val.str
+}
+
+func RString_length(vm *GobiesVM, receiver Object, v []Object) Object {
+	obj := receiver.(RObject)
+	arg := make([]Object, 1, 1)
+	arg[0] = int64(len(obj.val.str))
+	return RFixnum_new(vm, receiver, arg)
 }
