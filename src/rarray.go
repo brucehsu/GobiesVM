1d04b5dde925591453d335da931e8d7a95b84fb2
Szu-Kai Hsu (brucehsu) <brucehsu@gmail.com>
2014-07-21 20:47:36 +0800

    [RArray] each(&block) implemented

--- src/rarray.go
+++ src/rarray.go
@@ -18,6 +18,7 @@ func initRArray() *RObject {
 	obj.methods["inspect"] = &RMethod{gofunc: RArray_inspect}
 	obj.methods["size"] = &RMethod{gofunc: RArray_length}
 	obj.methods["length"] = &RMethod{gofunc: RArray_length}
+	obj.methods["each"] = &RMethod{gofunc: RArray_each}
 
 	return obj
 }
@@ -111,4 +112,26 @@ func RArray_length(vm *GobiesVM, receiver Object, v []Object) Object {
 	length[0] = int64(len(internal_array))
 
 	return RFixnum_new(vm, receiver, length)
+}
+
+// RArray.each(&block)
+// Given: [RBlock]
+// Block parameters: [item]
+func RArray_each(vm *GobiesVM, receiver Object, v []Object) Object {
+	obj := receiver.(*RObject)
+	if v != nil && len(v) == 1 { // Given a single RBlock
+		block := v[0].(*RObject)
+		internal_array := obj.ivars["array"].([]*RObject)
+
+		params := make([]*RObject, 1, 1)
+
+		for i := 0; i < len(internal_array); i++ {
+			// Prepare block arguments
+			params[0] = internal_array[i]
+
+			// Let VM handle all other stuff such as clean call frame
+			vm.executeBlock(block, params)
+		}
+	}
+	return obj
 }
